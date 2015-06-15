38a8bb6a248f3247853a4c9b85910e96cb9931bb
Justin Combs <combs.justin.m@gmail.com>
2015-06-14 21:39:20 -0400

    more icons and links in contact li

--- app/js/templates.js
+++ app/js/templates.js
@@ -4,15 +4,19 @@ this["template"]["contact"] = Handlebars.template({"1":function(depth0,helpers,p
 
   return "  <li class=\"dataItem\" id=\""
     + alias3(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"_id","hash":{},"data":data}) : helper)))
-    + "\">\n\n    "
+    + "\">\n    <i class=\"fa fa-user\"></i>\n    "
     + alias3(((helper = (helper = helpers.fullName || (depth0 != null ? depth0.fullName : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"fullName","hash":{},"data":data}) : helper)))
     + "\n\n    <span class=\"delete\" id=\"delete\"><i class=\"fa fa-trash\"></i></i></span>\n\n\n    <ul class='contactInfo'>\n      <li>\n        <i class=\"fa fa-phone\"></i>\n        "
     + alias3(((helper = (helper = helpers.phoneNumber || (depth0 != null ? depth0.phoneNumber : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"phoneNumber","hash":{},"data":data}) : helper)))
-    + "\n      </li>\n      <li>\n        <i class=\"fa fa-envelope-o\"></i>\n        "
+    + "\n      </li>\n      <li>\n        <i class=\"fa fa-envelope-o\"></i>\n        <a href=\"mailto:"
     + alias3(((helper = (helper = helpers.email || (depth0 != null ? depth0.email : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"email","hash":{},"data":data}) : helper)))
-    + "\n      </li>\n      <li>\n        <i class=\"fa fa-twitter\"></i>\n        "
+    + "\" target=\"_top\">"
+    + alias3(((helper = (helper = helpers.email || (depth0 != null ? depth0.email : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"email","hash":{},"data":data}) : helper)))
+    + "</a>\n      </li>\n      <li>\n        <i class=\"fa fa-twitter\"></i>\n        <a href=\"https://twitter.com/"
     + alias3(((helper = (helper = helpers.handle || (depth0 != null ? depth0.handle : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"handle","hash":{},"data":data}) : helper)))
-    + "\n      </li>\n      <li class=\"categoryList\">\n        <i class=\"fa fa-tag\"></i>\n        "
+    + "\" target=\"_blank\">@"
+    + alias3(((helper = (helper = helpers.handle || (depth0 != null ? depth0.handle : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"handle","hash":{},"data":data}) : helper)))
+    + "</a>\n      </li>\n      <li class=\"categoryList\">\n        <i class=\"fa fa-tag\"></i>\n        "
     + alias3(((helper = (helper = helpers.category || (depth0 != null ? depth0.category : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"category","hash":{},"data":data}) : helper)))
     + "\n      </li>\n    </ul>\n  </li>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
